cb6c8d14ed02041992d487c281653936bc81f8ef
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-20 20:32:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 // Write your solution here!
-const drivers=["Milo", "Otis", "Garfield"];
-
-function.destructivelyAppendDriver('name') {
-drivers.push('name')
-}
+const drivers=['Milo', 'Otis', 'Garfield'];
